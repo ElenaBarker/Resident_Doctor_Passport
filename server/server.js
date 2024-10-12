663617630316d51508ac8ddd657839422a8bd5d4
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2024-10-12 16:17:34 +0100

    created database folder with data.sql and db.js file

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
-const cors = require("cors");
-app.use(cors());
 
+const cors = require("cors");
+const port = process.env.PORT || 5005;
+const pool = require("./database/db");
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+pool.connect();
+
+app.listen(port, () => {
+  console.log(`Server is running on Port: ${port}`);
+});
 
 app.get("/", function (req, res) {
   res.send("Hello dream team :)");
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}. Ready to accept requests!`);
 });
